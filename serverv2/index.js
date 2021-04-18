7b701e3f8908d25192a18c73afb25e7354184642
Nicholas Apanian <nicholas.apanian@outlook.com>
2021-04-18 17:18:23 -0400

    fix query09

--- serverv2/index.js
+++ serverv2/index.js
@@ -541,15 +541,16 @@ app.post('/followUpForm', (req, res) => {
 
 /********************* /9 **********************/
 app.get('/query09', (req, res) => {
-  db.query("SELECT  p.first_name, p.last_name, group_concat(s.symptom) as 'Main Symptom', group_concat(ds.other) as 'Other Symptoms' FROM DiagnosticSymptoms as ds INNER JOIN Diagnostic as d ON ds.Diagnostic_id = d.id INNER JOIN Person as p ON d.person_id = p.id INNER JOIN Symptom as s ON ds.Symptom_id = s.id GROUP BY p.first_name;", function(error, results, fields){
-    if (error) {
-      console.log(error)
-      res.send({ success: false });
-    }
-    else {
-      res.send({ success: true, data: [...results] });
-    }
-  });
+    let {date, id} = req.query;
+    db.query("SELECT  p.first_name, p.last_name, group_concat(s.symptom) as 'Common Symptom', group_concat(ds.other) as 'Other Symptoms' FROM DiagnosticSymptoms as ds INNER JOIN Diagnostic as d ON ds.Diagnostic_id = d.id INNER JOIN Person as p ON d.person_id = p.id INNER JOIN Symptom as s ON ds.Symptom_id = s.id WHERE p.id = ? AND ds.date = ? GROUP BY p.first_name;", [id, date], function(error, results, fields){
+        if (error) {
+            console.log(error)
+            res.send({ success: false });
+        }
+        else {
+            res.send({ success: true, data: [...results] });
+        }
+    });
 })
 
 app.get('/infectedPeople', (req, res) => {
